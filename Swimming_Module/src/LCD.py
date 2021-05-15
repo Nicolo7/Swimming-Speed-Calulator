3a5365ffe6ba03cae1799a5a51adb7b3b77959b5
Nicolo7 <58084412+Nicolo7@users.noreply.github.com>
2021-05-15 14:40:00 -0400

    Updated LCD.py
    Modified original repo so this can be imported as a class for easier use and more organized code

--- Swimming_Module/src/LCD.py
+++ Swimming_Module/src/LCD.py
@@ -5,20 +5,8 @@
 ***
 
 #!/usr/bin/python
-# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
-
 import Adafruit_CharLCD as LCD
-
-
-# Raspberry Pi pin configuration:
-#lcd_rs        = 27  # Note this might need to be changed to 21 for older revision Pi's.
-#lcd_en        = 22
-#lcd_d4        = 25
-#lcd_d5        = 24
-#lcd_d6        = 23
-#lcd_d7        = 18
-#lcd_backlight = 4
 
 lcd_rs        = 25 
 lcd_en        = 24
@@ -27,74 +15,51 @@ lcd_d5        = 17
 lcd_d6        = 18
 lcd_d7        = 22
 lcd_backlight = 4
-lcd_columns = 16
-lcd_rows = 2
-
-# BeagleBone Black configuration:
-# lcd_rs        = 'P8_8'
-# lcd_en        = 'P8_10'
-# lcd_d4        = 'P8_18'
-# lcd_d5        = 'P8_16'
-# lcd_d6        = 'P8_14'
-# lcd_d7        = 'P8_12'
-# lcd_backlight = 'P8_7'
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows    = 2
 
-# Alternatively specify a 20x4 LCD.
-# lcd_columns = 20
-# lcd_rows    = 4
-
-# Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
-
-# Print a two line message
-lcd.message('Hello\nworld!')
-
-# Wait 5 seconds
-time.sleep(5.0)
-
-# Demo showing the cursor.
-lcd.clear()
-lcd.show_cursor(True)
-lcd.message('Show cursor')
-
-time.sleep(5.0)
-
-# Demo showing the blinking cursor.
-lcd.clear()
-lcd.blink(True)
-lcd.message('Blink cursor')
-
-time.sleep(5.0)
-
-# Stop blinking and showing cursor.
-lcd.show_cursor(False)
-lcd.blink(False)
-
-# Demo scrolling message right/left.
-lcd.clear()
-message = 'Scroll'
-lcd.message(message)
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_right()
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_left()
-
-# Demo turning backlight off and on.
-lcd.clear()
-lcd.message('Flash backlight\nin 5 seconds...')
-time.sleep(5.0)
-# Turn backlight off.
-lcd.set_backlight(0)
-time.sleep(2.0)
-# Change message.
-lcd.clear()
-lcd.message('Goodbye!')
-# Turn backlight on.
-lcd.set_backlight(1)
+class SwimLCD():
+    
+    def __init__(self):
+        
+        # Initialize the LCD using the pins above.
+        self.lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                                   lcd_columns, lcd_rows, lcd_backlight)
+    
+    def lcdShowMessage(self, firstline, secondline):
+        message = firstline
+        message += '\n'
+        message += 'secondline'
+        self.lcd.message(message)
+        return message
+        
+    def showLcdCursor(self):
+        self.lcd.show_cursor(True)
+        
+    def blinkLcdCursor(self):
+        self.lcd.blink(True)
+        
+    def staticLcdCursor(self):
+        self.lcd.blink(False)
+    
+    def hideLcdCursor(self):
+        self.lcd.show_cursor(False)
+        
+    def clearLcd(self):
+        self.lcd.clear()
+        
+    def scrollRight(self, firstline, secondline):
+        message = self.lcdShowMessage(firstline, secondline)
+        
+        for i in range(lcd_columns-len(message)):
+            time.sleep(0.5)
+            self.lcd.move_right()
+            
+    def scrollLeft(self, firstline, secondline):
+        message = self.lcdShowMessage(firstline, secondline)
+        
+        for i in range(lcd_columns-len(message)):
+            time.sleep(0.5)
+            self.lcd.move_left()
